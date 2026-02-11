bbe55067ead8cdde7de860de3220366fb742f786
cwinkler <cwinkler@mpcdf.mpg.de>
2026-02-11 15:53:53 +0100

    update log to file function

--- experimental-simulation-tc/src/log_to_file.py
+++ experimental-simulation-tc/src/log_to_file.py
@@ -3,8 +3,14 @@ from contextlib import redirect_stdout
 def log_output(file_path):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            with open(file_path, "a") as f:
-                with redirect_stdout(f):
-                    return func(*args, **kwargs)
-        return wrapper
+            try:
+                with open(file_path, "a") as f:
+                    with redirect_stdout(f):
+                        return func(*args, **kwargs)
+            except FileNotFoundError:
+                return func(*args, **kwargs)
+
+        return wrapper 
+
     return decorator
+
