78c2bce67b7e4612ea5e4bd262084b171f57859c
Andreas Marek <andreas.marek@mpcdf.mpg.de>
2026-06-25 08:44:40 +0200

    Removed experimental-simulation-ms in parent directory

--- experimental-simulation-ms/src/utils.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def get_git_commit_hash():
-    try:
-        return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
-    except Exception:
-        return "unknown"
